cbb1d287af95ad3f28f40c2fa5b0afc8f0028678
nick van Oijen <I468166@fhict.nl>
2021-04-02 14:53:34 +0200

    feature added tons of updates

---
+++ chirpel-react/src/Messages/Messages.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class Messages extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render()
+    {
+        <div>
+            
+        </div>
+    }
+}
+export default Messages
